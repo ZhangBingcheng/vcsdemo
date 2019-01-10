b2d8bcb70bbd6ee55dd9a9aa7024d4ec74b60687
ZhangBingcheng <43721575+ZhangBingcheng@users.noreply.github.com>
2019-01-10 19:52:21 +0800

    改7为5
    2019-1-10 19:52:19

--- src/main/java/com/jnshu/Test.java
+++ src/main/java/com/jnshu/Test.java
@@ -5,6 +5,6 @@ public class Test {
         System.out.println ("Hello world!");
         System.out.println ("first");
         System.out.println (9999999);
-        System.out.println (7777);
+        System.out.println (5555);
     }
 }
