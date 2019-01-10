dc88b28a1945b11de8b223bbe5c3b485203430a2
ZhangBingcheng <43721575+ZhangBingcheng@users.noreply.github.com>
2019-01-10 15:44:37 +0800

    五

--- src/main/java/com/jnshu/Test.java
+++ src/main/java/com/jnshu/Test.java
@@ -3,5 +3,7 @@ package com.jnshu;
 public class Test {
     public static void main(String[] args) {
         System.out.println ("Hello world!");
+        System.out.println ("first");
+        System.out.println (2222222);
     }
 }
