ba4f10ae961ac320246719be080ef72eb04ea077
ZhangBingcheng <43721575+ZhangBingcheng@users.noreply.github.com>
2019-01-10 15:48:43 +0800

    修改
    把2改为9

--- src/main/java/com/jnshu/Test.java
+++ src/main/java/com/jnshu/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println ("Hello world!");
         System.out.println ("first");
-        System.out.println (2222222);
+        System.out.println (9999999);
     }
 }
