024be025dad4963e41d460767847b9dd0d9b6b4f
ZhangBingcheng <43721575+ZhangBingcheng@users.noreply.github.com>
2019-01-10 18:16:37 +0800

    66666

--- src/main/java/com/jnshu/Test.java
+++ src/main/java/com/jnshu/Test.java
@@ -5,6 +5,6 @@ public class Test {
         System.out.println ("Hello world!");
         System.out.println ("first");
         System.out.println (9999999);
-        int i=123;
+        System.out.println (666666);
     }
 }
